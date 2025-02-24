b538d585ed55a7488af5eca7d9b5fd18aa127ce3
Spyros Zarkadis <spyroszarkadis@gmail.com>
2025-02-24 11:12:47 +0200

    Changing from switch statement to if else

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
+let humanScore = 0;
+let computerScore = 0;
+
 
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
-    console.log(computerChoice);
+
+     if (computerChoice = 0){
+        return "Rock";
+     } else if (computerChoice = 1) {
+        return "Paper";
+     } else { 
+        return "Scissors"; 
+     }
+     console.log(computerChoice);
 }
 
 
 function getHumanChoice() {
-    let humanChoice = prompt("Choose your number!");
+    let humanChoice = prompt("Choose between Rock, Paper or Scissors!");
+
+    if (humanChoice = "Rock") {
+        return "Your choice is Rock";
+    } else if (humanChoice = "Paper.") {
+        return "Your choice is Paper.";
+    } else {
+        return "Your choice is Scissors."
+    }
+
     console.log(humanChoice);
 }
-
-let humanScore = 0;
-let computerScore = 0;
